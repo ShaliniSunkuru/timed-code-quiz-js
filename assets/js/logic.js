b3bac1e6b7c059be1ef52deeb569388100b61936
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-01-12 11:38:20 +0000

    create choices buttons for question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,8 @@
 var timeEl = document.querySelector("#time");
 var startBtn = document.querySelector("#start");
+var questionsDiv = document.querySelector("#questions")
+var questionH2 = document.querySelector("#question-title")
+var choicesDiv = document.querySelector("#choices");
 
 function setTimer(){
     var timeLeft = 60;
@@ -19,10 +22,28 @@ function setTimer(){
 }
 
 startBtn.addEventListener("click", function(){
+   
     //render first question
+    renderQuestion(questions[0]);
 
     //Start timer
     setTimer();
 })
 
+function renderQuestion(question){
+    
+    //Show Question section: change class from hide to start
+    questionsDiv.className = "start";
 
+    //Question title
+    questionH2.textContent = question.questionTitle;
+
+    // Choice buttons for the question
+    choicesDiv.setAttribute("style", "display: block");
+    question.choices.forEach((element, index) => {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.textContent = (index + 1) + ". " + element;
+        choicesDiv.append(choiceBtn);
+        
+    });
+}
