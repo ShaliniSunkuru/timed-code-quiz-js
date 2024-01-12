f7d97d995b74e535416112e37af14a29e7bae114
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-01-12 10:35:35 +0000

    changed . to , in array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -29,4 +29,4 @@ var question6 = {
     correctChoice: 2
 }
 
-var questions = [question1, question2, question3. question4, question5, question6];
+var questions = [question1, question2, question3, question4, question5, question6];
